a63da20cb4328d7f124e58e22920dc71ca16e0a1
zhaifenghw <zhaifeng@huawei.com>
2021-09-28 18:53:37 +0800

    fix check usb issues
    Signed-off-by: zhaifenghw <zhaifeng@huawei.com>

--- src/common/base.cpp
+++ src/common/base.cpp
@@ -20,6 +20,7 @@
 #include <openssl/buffer.h>
 #include <openssl/evp.h>
 #include <openssl/md5.h>
+#include <sstream>
 #include <thread>
 #ifdef __MUSL__
 extern "C" {
@@ -373,9 +374,11 @@ namespace Base {
     {
         srand(static_cast<unsigned int>(GetRandom()));
         string ret = string(expectedLen, '0');
+        std::stringstream val;
         for (auto i = 0; i < expectedLen; ++i) {
-            sprintf(&ret[i], "%X", rand() % BUF_SIZE_MICRO);
+            val << std::hex << (rand() % BUF_SIZE_MICRO);
         }
+        ret = val.str();
         return ret;
     }
 
